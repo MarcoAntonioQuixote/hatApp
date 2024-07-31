e7a1553ab13e11f06ab0d0f3c008988e7d4d821a
Mark Anthony Gutierrez <mr.markgutierrez@gmail.com>
2024-07-30 18:34:07 -0600

    Practicing State with Hat App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+
+    const isCustomized = false
+
+    const [qty, setQty] = useState(0);
+    const [hatObj, setHatObj] = useState({
+        brandName: '',
+        size: 0,
+        color: 'black',
+        text: ''
+    })
+    //is it even customized? 
+
+    //on a submit (when they order)
+    //log what the hat looks like (brandName,size,color,isEmbroidered,text)
+
+    //log how many hats of that kind they want
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header'>
+        Hats 🎩
+
+        The color of the hat you are ordering is {hatObj.color}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+// {isCustomized ? <input placeholder='what do you want the hat to say' type='text' /> : null}
